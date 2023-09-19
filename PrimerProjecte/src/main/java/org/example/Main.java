ca96b8222598c51f982f5f939d5378b599c46ed9
nereheirodriguez <nereheirodriguez@iesebre.com>
2023-09-19 13:15:32 +0200

    he afegit el primer projecte Java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Inici del comentari de mes d'una linia
+        int j= 0;
+        int a= 9;
+        double d = 98;
+        Final del comentari de mes d'una linea*/
+        System.out.println("Hola Nerei!");
+        System.out.println("Segona linea de text");
+        System.out.print("Me dic Nerei");
+        System.out.println("I soc l'alumne de DAM");
+
+    }
+
+}
